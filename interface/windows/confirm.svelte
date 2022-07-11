1a8cdf0a90ea00826d814f30f5dccc705e475abc
Levminer <levminer@levminer.com>
2022-07-11 14:19:09 +0200

    Confirm window design

---
+++ interface/windows/confirm.svelte
@@ -0,0 +1,76 @@
+<div class="transparent-900 m-auto mt-40 mb-60 w-3/5 rounded-2xl p-10 text-center">
+	<h1>Welcome back!</h1>
+
+	<div class="mx-auto flex w-4/5 flex-col items-center justify-center rounded-2xl p-10">
+		<div class="transparent-800 mb-10 w-full rounded-2xl p-5">
+			<h2>Confirm password</h2>
+			<h3>Please enter your password to continue.</h3>
+		</div>
+
+		<div class="transparent-800 mb-10 w-full rounded-2xl p-5">
+			<div class="mx-auto flex w-full items-center justify-center text-left">
+				<div>
+					<h5>Password</h5>
+					<div class="flex items-center justify-center">
+						<input class="input passwordInput mt-1" type="password" />
+						<svg xmlns="http://www.w3.org/2000/svg" on:click={showPassword} class="showPassword0 relative right-9 h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+							<path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+						</svg>
+						<svg xmlns="http://www.w3.org/2000/svg" on:click={showPassword} class="hidePassword0 relative right-9 hidden h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
+						</svg>
+					</div>
+				</div>
+			</div>
+
+			<button class="button mx-auto mt-10">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+				</svg>
+				Confirm
+			</button>
+		</div>
+
+		<Details>
+			<div class="flex items-center justify-between">
+				<div>
+					<h4>Forgot password?</h4>
+					<h5>Your codes are protected by your password. If you forgot your password you can't access your codes.</h5>
+				</div>
+				<div>
+					<button class="smallButton">
+						<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+						</svg>
+						More information
+					</button>
+				</div>
+			</div>
+		</Details>
+	</div>
+</div>
+
+<script>
+	import Details from "../components/details.svelte"
+
+	let passwordShown = false
+
+	const showPassword = () => {
+		if (passwordShown === false) {
+			document.querySelector(".showPassword0").style.display = "none"
+			document.querySelector(".hidePassword0").style.display = "block"
+
+			document.querySelector(".passwordInput").setAttribute("type", "text")
+
+			passwordShown = true
+		} else {
+			document.querySelector(".showPassword0").style.display = "block"
+			document.querySelector(".hidePassword0").style.display = "none"
+
+			document.querySelector(".passwordInput").setAttribute("type", "password")
+
+			passwordShown = false
+		}
+	}
+</script>
